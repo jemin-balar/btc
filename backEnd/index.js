e9fe53946e3a0ab8487f6a9fcf798af9a14c104d
Jemin_balar <124352839+jemin-balar@users.noreply.github.com>
2024-12-05 19:23:13 +0530

    Update index.js

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,18 +1,26 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const connectDB = require('./connection');
+// const userRoutes = require('./routes/user.routes');
+// const dotenv = require('dotenv');
+// require('dotenv').config();
+// dotenv.config();
+// const port = 9000;
+
+// app.use(express.json());
+
+// connectDB('mongodb://localhost:27017/userDB').then(() => console.log('Connected to MongoDB'));
+
+// app.use("/user", userRoutes);
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+const express = require("express");
 const app = express();
-const connectDB = require('./connection');
-const userRoutes = require('./routes/user.routes');
-const dotenv = require('dotenv');
-require('dotenv').config();
-dotenv.config();
-const port = 9000;
 
-app.use(express.json());
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-connectDB('mongodb://localhost:27017/userDB').then(() => console.log('Connected to MongoDB'));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.use("/user", userRoutes);
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
