9b1ecd2a7a6ac240e714a300eed8bc769d0959b9
jemin-balar <balarjemin701@gmail.com>
2024-12-11 13:49:06 +0530

    changes

--- backEnd/index.js
+++ backEnd/index.js
@@ -9,7 +9,7 @@ const port = 9000;
 
 app.use(express.json());
 
-connectDB('mongodb://localhost:27017/userDB').then(() => console.log('Connected to MongoDB'));
+connectDB('mongodb+srv://balarjemin701:NIbdBFj85j0OmEYM@cluster0.ysgez0m.mongodb.net/btc?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('Connected to MongoDB'));
 
 app.use("/user", userRoutes);
 
