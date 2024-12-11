d804c1c367f9f6f2ea6c369f1cd11401ee693516
jemin-balar <balarjemin701@gmail.com>
2024-12-11 14:42:43 +0530

    add cors

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const connectDB = require('./connection');
 const userRoutes = require('./routes/user.routes');
+const cors = require('cors');
 const dotenv = require('dotenv');
 require('dotenv').config();
 dotenv.config();
 const port = 9000;
 
 app.use(express.json());
+app.use(cors());
 
 connectDB('mongodb+srv://balarjemin701:NIbdBFj85j0OmEYM@cluster0.ysgez0m.mongodb.net/btc?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('Connected to MongoDB'));
 
